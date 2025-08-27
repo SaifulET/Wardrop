3b5415be3b49644d00917ef0e31efabca850f32e
SaifulET <si912999@gmail.com>
2025-08-28 05:13:03 +0600

    dkadds

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 const CategorySchema = new Schema({
   // user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  category: { type: String, required: true,unique: true },
+  name: { type: String, required: true,unique: true },
   subCategory: { type: String ,unique: true },
   subsubCategory: { type: String, unique: true }, 
   items: { type: Schema.Types.ObjectId, ref: "Item" }
