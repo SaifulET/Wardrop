2150f6c8513e851ee378292beb2aa718a0bdacf0
SaifulET <si912999@gmail.com>
2025-08-28 03:39:36 +0600

    dkadds

--- src/models/Item.js
+++ src/models/Item.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const itemSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
+  title: { type: String},
   brand: { type: String },
   category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
   material: [{ type: Schema.Types.ObjectId, ref: "Material"}],        
