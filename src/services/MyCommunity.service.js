7b9f2ed66680ff535a30fe0ed1866f25d6bb692f
SaifulET <si912999@gmail.com>
2025-09-11 23:33:48 +0600

    kdl

--- src/services/MyCommunity.service.js
+++ src/services/MyCommunity.service.js
@@ -2,14 +2,14 @@ import User from "../models/User.js";
 
 // 1. Get following info
 export const getFollowingService = async (userId) => {
-  const user = await User.findById(userId).populate("following", "_id name username email image");
+  const user = await User.findById(userId).populate("following", "_id name username email profileImage");
   if (!user) throw new Error("User not found");
   return user.following;
 };
 
 // 2. Get followers info
 export const getFollowersService = async (userId) => {
-  const user = await User.findById(userId).populate("followers", "_id name username email image");
+  const user = await User.findById(userId).populate("followers", "_id name username email profileImage");
   if (!user) throw new Error("User not found");
   return user.followers;
 };
@@ -20,9 +20,8 @@ export const getNonConnectedUsersService = async (userId) => {
   if (!user) throw new Error("User not found");
 
   const excludedIds = [...user.followers, ...user.following, user._id]; // exclude followers, following & self
-
   const nonConnectedUsers = await User.find({ _id: { $nin: excludedIds } })
-    .select("_id name username email image");
+    .select("_id name username email profileImage");
 
   return nonConnectedUsers;
 };
